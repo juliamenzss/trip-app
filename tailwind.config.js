22085499de509786197a071347e0748538548a38
julia <juliamenzss@gmail.com>
2025-05-11 15:40:40 -0300

    style: adiciona variáveis cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: [
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#dc97c2', 
+        secondary: '#c77daf',
+        background: '#f4efec',
+        text: '#1F2937', 
+        redColor: '#F85572', 
+      },
+    },
   },
   plugins: [],
 }
-
