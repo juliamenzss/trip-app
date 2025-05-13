c3ed4c41128e638bf105a4d6e7f855efa3d3ea36
julia <juliamenzss@gmail.com>
2025-05-13 09:43:53 -0300

    feat: adiciona lógica de props em modulos em layout

--- src/app/template/layout/layout.component.ts
+++ src/app/template/layout/layout.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { LayoutProps } from './layoutProps';
+import { ActivatedRoute, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
   standalone: false,
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.scss'
+  styleUrl: './layout.component.scss',
 })
 export class LayoutComponent {
+  props!: LayoutProps;
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
 
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        filter(() => this.activatedRoute.firstChild !== null),
+        map(() => this.obterProps())
+      )
+      .subscribe((props: LayoutProps) => (this.props = props));
+  }
+
+  obterProps(): LayoutProps {
+    let rotaFilha = this.activatedRoute.firstChild;
+    while (rotaFilha?.firstChild) {
+      rotaFilha = rotaFilha.firstChild;
+    }
+    return rotaFilha?.snapshot.data as LayoutProps;
+  }
 }
